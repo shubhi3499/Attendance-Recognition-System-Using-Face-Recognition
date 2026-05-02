db407815de61812da5332dcda4509b399c9eadf6
Shubham Zawar <shubhamzawar55@gmail.com>
2026-05-02 16:15:12 +0530

    Created Register and Login from for teacher

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,5 +1,67 @@
 import streamlit as st
-
+from src.ui.base_layout import style_background_dashboard,style_base_layout
+from src.components.header import header_dashboard
 
 def teacher_screen():
-    st.header('Teacher screen')
+    style_background_dashboard()
+    style_base_layout()
+
+    if "teacher_login_type" not in st.session_state or st.session_state.teacher_login_type=="login":
+        teacher_screen_login()
+    elif st.session_state.teacher_login_type == "register":
+        teacher_screen_register()
+
+def teacher_screen_login():
+    c1,c2 = st.columns(2,vertical_alignment='center',gap='large')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button('Go back to Home',type='secondary',key='loginbackbtn',shortcut="control+backspace"):
+            st.session_state['login_type'] = None
+            st.rerun()
+    
+    
+    
+    st.header('Login using Password',text_alignment='center')
+    st.space()
+    teacher_username = st.text_input("Enter username",placeholder="enter username")
+
+    teacher_password = st.text_input("Enter password",type='password',placeholder="enter password")
+
+    st.divider()
+
+    btnc1,btnc2 = st.columns(2)
+
+    with btnc1:
+        st.button('Login',icon=":material/passkey:",shortcut="control+enter",width="stretch")
+    with btnc2:
+        if st.button('Register Instead',type="primary",icon=":material/passkey:",width="stretch"):
+            st.session_state.teacher_login_type = 'register'
+
+
+
+def teacher_screen_register():
+    c1,c2 = st.columns(2,vertical_alignment='center',gap='large')
+    with c1:
+         header_dashboard()
+    with c2:
+         st.button('Go back to Home',type='secondary',key='loginbackbtn',shortcut="control+backspace")
+   
+    st.header("Register Teacher Profile")
+    teacher_username = st.text_input("Enter username",placeholder="enter username")
+
+    teacher_name = st.text_input("Enter name",placeholder="enter your name")
+
+    teacher_password = st.text_input("Enter password",type='password',placeholder="enter password")
+
+    teacher_password_confirm = st.text_input("Confirm your password",type='password',placeholder="re-enter password")
+
+    st.divider()
+
+    btnc1,btnc2 = st.columns(2)
+
+    with btnc1:
+        st.button('Register Now',icon=":material/passkey:",shortcut="control+enter",width="stretch")
+    with btnc2:
+        if st.button('Login Instead',type="primary",icon=":material/passkey:",width="stretch"):
+            st.session_state.teacher_login_type = 'login'
